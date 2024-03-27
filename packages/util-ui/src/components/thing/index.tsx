ce8f0629342bfda85fa8753d95651941d479d313
sampaiogabriel <gabrielsampaiolima@hotmail.com>
2024-03-27 08:12:38 -0300

    initial setup

---
+++ packages/util-ui/src/components/thing/index.tsx
@@ -0,0 +1,7 @@
+import { FC } from "react";
+
+const Thing: FC = () => {
+    return <div>Things</div>
+}
+
+export default Thing;
---
+++ packages/util-ui/src/sampaiogabriel-util-ui.tsx
@@ -0,0 +1,3 @@
+import Thing from './components/thing'
+
+export { Thing }
