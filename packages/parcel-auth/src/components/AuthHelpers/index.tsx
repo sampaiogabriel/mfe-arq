20fece8a8cc6f6944d1d93460408088c459a2245
sampaiogabriel <gabrielsampaiolima@hotmail.com>
2024-03-28 07:35:06 -0300

    feat - add eslint no parcel auth

--- packages/parcel-auth/src/components/AuthHelpers/index.tsx
+++ packages/parcel-auth/src/components/AuthHelpers/index.tsx
@@ -4,39 +4,39 @@ import { hasAuthParams, useAuth } from 'react-oidc-context';
 import { useUserStore } from '@sampaiogabriel/util-state';
 
 const AuthHelpers = () => {
-    const auth = useAuth();
-    const { setUser } = useUserStore();
-    const [hasTriedSignin, setHasTriedSignin] = useState(false);
+  const auth = useAuth();
+  const { setUser } = useUserStore();
+  const [hasTriedSignin, setHasTriedSignin] = useState(false);
 
-    useEffect(() => {
-        if (!hasAuthParams() &&
-            !auth.isAuthenticated && !auth.activeNavigator && !auth.isLoading &&
-            !hasTriedSignin
-        ) {
-            auth.signinRedirect();
-            setHasTriedSignin(true);
+  useEffect(() => {
+    if (!hasAuthParams() &&
+      !auth.isAuthenticated && !auth.activeNavigator && !auth.isLoading &&
+      !hasTriedSignin
+    ) {
+      auth.signinRedirect();
+      setHasTriedSignin(true);
+    }
+
+    if (auth.isAuthenticated) {
+      const { id_token, access_token, refresh_token, profile } = auth.user;
+
+      const user = {
+        id_token: id_token,
+        access_token: access_token,
+        refresh_token: refresh_token,
+        profile: {
+          email: profile.email,
+          name: profile.name,
+          given_name: profile.given_name,
+          family_name: profile.family_name,
         }
+      }
 
-        if (auth.isAuthenticated) {
-            const { id_token, access_token, refresh_token, profile } = auth.user;
+      setUser(user)
+    }
+  }, [auth, hasTriedSignin]);
 
-            const user = {
-                id_token: id_token,
-                access_token: access_token,
-                refresh_token: refresh_token,
-                profile: {
-                    email: profile.email,
-                    name: profile.name,
-                    given_name: profile.given_name,
-                    family_name: profile.family_name,
-                }
-            }
-
-            setUser(user)
-        }
-    }, [auth, hasTriedSignin]);
-
-    return <></>;
+  return <></>;
 };
 
 export default AuthHelpers;
--- packages/parcel-auth/src/root.component.tsx
+++ packages/parcel-auth/src/root.component.tsx
@@ -4,22 +4,23 @@ import { AuthProvider } from 'react-oidc-context';
 import AuthHelpers from './components/AuthHelpers';
 
 export const oidcConfig = {
-    authority: 'https://login.e-auditoria.com.br/realms/aplicacao-cliente',
-    client_id: 'client-cliente',
-    redirect_uri: 'http://localhost:9000',
-    client_secret: 'EldjqZ7LDThiQ0f4DuLxIIf38VyilZdP',
+  authority: 'https://login.e-auditoria.com.br/realms/aplicacao-cliente',
+  client_id: 'client-cliente',
+  redirect_uri: 'http://localhost:9000',
+  client_secret: 'EldjqZ7LDThiQ0f4DuLxIIf38VyilZdP',
 };
 
 interface AuthProviderComponentProps {
-    children: ReactNode
+  children: ReactNode
 }
 
 const Root: FC<AuthProviderComponentProps> = ({ children }): ReactElement => {
-    return (
-        <AuthProvider {...oidcConfig}>
-            <AuthHelpers />
-        </AuthProvider>
-    )
+  return (
+    <AuthProvider {...oidcConfig}>
+      <AuthHelpers />
+      {children}
+    </AuthProvider>
+  )
 };
 
 export default Root;
